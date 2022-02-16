ddc4007a838ba632e9b2462534064bc151fab0f3
micheaol <micheaol80@gmail.com>
2022-02-16 20:18:54 +0100

    Add test for posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+  describe 'GET #index' do
+    before(:each) do
+      get user_posts_path(745)
+    end
+
+    it 'should have the correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
 end
