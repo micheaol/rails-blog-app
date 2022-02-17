0e7963fda31f2d507f7105e2716c9df79ec39a2e
micheaol <micheaol80@gmail.com>
2022-02-17 21:30:34 +0100

    Create data model for the tables

--- db/migrate/20220217192951_create_users.rb
+++ db/migrate/20220217192951_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :photo
-      t.string :bio
-      t.integer :post_counter
+      t.string :name, null: false
+      t.string :photo, null: false, default: 'https://avatars.githubusercontent.com/u/10332499?v=4'
+      t.string :bio, null: false
+      t.integer :post_counter, default: 0, null: false
 
       t.timestamps
     end
