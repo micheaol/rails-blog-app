72fa9713816bb7a28f5276a136cdbf23a8fa1265
micheaol <micheaol80@gmail.com>
2022-02-22 03:59:00 +0100

    Add method to that returns the 3 most recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :comments
     has_many :posts
     has_many :likes
+
+    def recent_posts(limit = 3)
+        posts.order(created_at: :desc).limit(limit)
+    end
 end
