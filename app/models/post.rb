9c87520ab45b96f326bce9d4029d6906a07d8d34
micheaol <micheaol80@gmail.com>
2022-02-22 04:04:36 +0100

    Add method to updates the posts counter and a method which returns the 5 most recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_many :likes
+
+    def update_post_counter(counter)
+        user.update('post_counter', counter)
+    end
+
+    def most_recent_comment(limit = 5)
+        comments.order(created_at: :desc).limit(limit)
+    end
 end
