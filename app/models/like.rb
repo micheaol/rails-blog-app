3890ed4dcb62537a016961b4540fd87fa9c66e6c
micheaol <micheaol80@gmail.com>
2022-02-23 10:03:41 +0100

    Add style to post

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ApplicationRecord
   belongs_to :post
 
   def update_likes_counter(counter)
-    post.update('likes_counter', counter)
+    post.update(likes_counter: counter)
   end
 end
