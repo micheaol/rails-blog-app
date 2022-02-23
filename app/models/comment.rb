7dca22ff789a4f010a9e4f49ece58ac8aa05fee3
micheaol <micheaol80@gmail.com>
2022-02-23 09:44:59 +0100

    Add styling to the view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   def update_comment_counter(counter)
-    comments.update('comments_counter', counter)
+    post.update(comments_counter: counter)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_like_counter(counter)
-    likes.update('likes_counter', counter)
+  def update_likes_counter(counter)
+    post.update('likes_counter', counter)
   end
 end
