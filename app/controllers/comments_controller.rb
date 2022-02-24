1929e048e75937b778e4c5bbcffa5f065ed0bd08
micheaol <micheaol80@gmail.com>
2022-02-24 17:05:22 +0100

    Add routes for new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class CommentsController < ApplicationController
   def create
     post = Post.find(params[:id])
@@ -16,3 +17,28 @@ class CommentsController < ApplicationController
     params.require(:data).permit(:text)
   end
 end
+=======
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+    def create
+        post = Post.find(params[:id])
+        new_comment = Comment.create(user_id: current_user.id, post_id: post.id, text: comment_params[:text])
+        if new_comment.save
+            flash[:notice] = "Comment added successfully"
+          redirect_to user_post_path(post.user.id, post.id)
+        else
+            flash[:alert] = 'Failed to add comment!'
+          redirect_to user_post_path(post.user.id, post.id)
+        end
+      end
+    
+      private
+    
+      def comment_params
+        params.require(:data).permit(:text)
+      end
+end
+>>>>>>> a5dc7c5 (Add routes for new comment)
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   root 'users#index'
 
   post 'users/:id/like/create', to: 'likes#create', as: 'like_form'
+  get  'users/posts/:post_id/comments/new', to: 'comments#new', as: 'post_comment'
   post 'users/:id/comment/create', to: 'comments#create', as: 'comment_form'
-  get 'users/:user_id/posts/new', to: 'posts#new', as: 'user_new_post'
+  get  'users/:user_id/posts/new', to: 'posts#new', as: 'user_new_post'
   post 'users/:user_id/post', to: 'posts#create', as: 'create_new_post'
 
   resources :users, only: [:index, :show] do
