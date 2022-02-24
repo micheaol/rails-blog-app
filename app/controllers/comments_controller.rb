d4317ec98b16b52f51c84ee9ee0660ff05d52cb0
micheaol <micheaol80@gmail.com>
2022-02-24 17:12:37 +0100

    Add routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-class CommentsController < ApplicationController
-  def create
-    post = Post.find(params[:id])
-    new_comment = Comment.create(user_id: current_user.id, post_id: post.id, text: comment_params[:text])
-    if new_comment.save
-      flash[:notice] = 'Comment added successfully'
-    else
-      flash[:alert] = 'Failed to add comment!'
-    end
-    redirect_to user_post_path(post.user.id, post.id)
-  end
-
-  private
-
-  def comment_params
-    params.require(:data).permit(:text)
-  end
-end
-=======
 class CommentsController < ApplicationController
 
   def new
@@ -41,4 +21,3 @@ class CommentsController < ApplicationController
         params.require(:data).permit(:text)
       end
 end
->>>>>>> a5dc7c5 (Add routes for new comment)
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+  
   def create
     post = Post.find(params[:id])
     new_like = Like.create(user_id: current_user.id, post_id: post.id)
