ef996cfa4e1d6aa6c3d89c4404711d0ae39cf986
micheaol <micheaol80@gmail.com>
2022-02-24 11:08:23 +0100

    Add post form and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,34 @@ class PostsController < ApplicationController
     @posts = @user.recent_posts
   end
 
+
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
     @comments = @post.comments.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @current_user = User.find(params[:user_id])
+    @new_post = Post.new(user: @current_user, title: params[:post][:title], text: params[:post][:text])
+
+    if @new_post.save
+      flash[:notice] = "Post created successfully!!!"
+      redirect_to user_posts_path(@new_post.user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   root 'users#index'
 
+  get 'users/:user_id/posts/new', to: 'posts#new', as: 'user_new_post'
+  post 'users/:user_id/post', to: 'posts#create', as: 'create_new_post'
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] 
   end
