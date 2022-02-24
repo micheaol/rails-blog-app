2a6bc7aba4bca5b9ae7ca8a0e9f0308f193c7b2b
micheaol <micheaol80@gmail.com>
2022-02-24 14:53:00 +0000

    Fix linter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,18 @@
 class CommentsController < ApplicationController
-    def create
-        post = Post.find(params[:id])
-        new_comment = Comment.create(user_id: current_user.id, post_id: post.id, text: comment_params[:text])
-        if new_comment.save
-            flash[:notice] = "Comment added successfully"
-          redirect_to user_post_path(post.user.id, post.id)
-        else
-            flash[:alert] = 'Failed to add comment!'
-          redirect_to user_post_path(post.user.id, post.id)
-        end
-      end
-    
-      private
-    
-      def comment_params
-        params.require(:data).permit(:text)
-      end
+  def create
+    post = Post.find(params[:id])
+    new_comment = Comment.create(user_id: current_user.id, post_id: post.id, text: comment_params[:text])
+    if new_comment.save
+      flash[:notice] = 'Comment added successfully'
+    else
+      flash[:alert] = 'Failed to add comment!'
+    end
+    redirect_to user_post_path(post.user.id, post.id)
+  end
+
+  private
+
+  def comment_params
+    params.require(:data).permit(:text)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,6 @@ class PostsController < ApplicationController
     @posts = @user.recent_posts
   end
 
-
   def show
     @user = User.find(params[:id])
     @post = @user.posts.find(params[:user_id])
@@ -20,7 +19,7 @@ class PostsController < ApplicationController
     @new_post = Post.new(user: @current_user, title: params[:post][:title], text: params[:post][:text])
 
     if @new_post.save
-      flash[:notice] = "Post created successfully!!!"
+      flash[:notice] = 'Post created successfully!!!'
       redirect_to user_posts_path(@new_post.user.id)
     else
       render :new
@@ -32,6 +31,4 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :text)
   end
-
-
 end
