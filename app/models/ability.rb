7f0a79488ca034a38da170288c9b074b58fc93a9
micheaol <micheaol80@gmail.com>
2022-03-03 16:45:22 +0000

    Fix linter

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-      user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
-        # can :manage, Post, user_id: user_id
-        # can :manage, Comment, user_id: user_id
-        can :read, :all
-      end
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      # can :manage, Post, user_id: user_id
+      # can :manage, Comment, user_id: user_id
+      can :read, :all
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-    factory :comment do
-      user
-      post
-      text { 'Comment for the Test' }
-    end
+  factory :comment do
+    user
+    post
+    text { 'Comment for the Test' }
   end
+end
