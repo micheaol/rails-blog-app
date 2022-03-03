ffc9329aa3b5a585e61d9f642314fdde56ef2aeb
micheaol <micheaol80@gmail.com>
2022-03-03 15:29:35 +0100

    Add capybara to the project

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "spec_helper"
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -26,6 +27,8 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
+
+
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
@@ -37,10 +40,10 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
-  config.use_active_record = false
+  # config.use_active_record = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
